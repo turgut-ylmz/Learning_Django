51e1df291cf88c84f52b833f38e014050ea7968f
Turgut Yılmaz <101890137+turgut-ylmz@users.noreply.github.com>
2023-05-11 11:49:29 +0300

    Update serializers.py

--- 022_FlightAppSwaggerDebugToolbar/users/serializers.py
+++ 022_FlightAppSwaggerDebugToolbar/users/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers, validators
-# from django.contrib.auth.models import User
-# from django.conf import settings
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from dj_rest_auth.serializers import TokenSerializer
