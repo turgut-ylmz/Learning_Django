613829e4c78761813fe21be7ea57a8074943d355
Turgut Yılmaz <101890137+turgut-ylmz@users.noreply.github.com>
2023-05-16 13:03:11 +0300

    Update signals.py

--- 022_FlightAppSwaggerDebugToolbar/users/signals.py
+++ 022_FlightAppSwaggerDebugToolbar/users/signals.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-
-
 @receiver(post_save, sender=User)
 def create_token(sender, instance=None, created=False, **kwargs):
     if created:
